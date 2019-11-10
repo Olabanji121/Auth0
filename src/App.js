6b943eabe6b47c54a475c29487c84a01fc1fb5fa
Olabanji121 <olabanjiabimbola@gmail.com>
2019-11-10 10:35:58 +0100

    stable code

---
+++ src/App.js
@@ -0,0 +1,36 @@
+// src/App.js
+
+import React from "react";
+import NavBar from "./components/NavBar";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Profile from "./components/Profile";
+import { useAuth0 } from "./react-auth0-spa";
+import './App.css';
+import PrivateRoute from "./components/PrivateRoute";
+
+function App() {
+  const { loading } = useAuth0();
+
+  if (loading) {
+    return <div>Loading Details...</div>;
+  }
+
+  return (
+    
+      <div className="App">
+        {/* New - use BrowserRouter to provide access to /profile */}
+        <BrowserRouter>
+          <header>
+            <NavBar />
+          </header>
+          <Switch>
+            <Route path="/" exact />
+            <PrivateRoute path="/profile" component={Profile} />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    
+  );
+}
+
+export default App;
